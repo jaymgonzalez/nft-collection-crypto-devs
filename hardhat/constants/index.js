0050e1205e113ef58ba59d070a606f85bb60bfcb
Jay M Gonzalez <jaimungon@gmail.com>
2022-10-25 10:31:09 +0200

    fix issues with exports

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,3 +1,7 @@
-export const WHITELIST_CONTRACT_ADDRESS =
-  '0xA0b4F2b717c1fCC4F803258ACC0314e159F7a5Fd'
-export const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/'
+const WHITELIST_CONTRACT_ADDRESS = '0xA0b4F2b717c1fCC4F803258ACC0314e159F7a5Fd'
+const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/'
+
+module.exports = {
+  WHITELIST_CONTRACT_ADDRESS,
+  METADATA_URL,
+}
