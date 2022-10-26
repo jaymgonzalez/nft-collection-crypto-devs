2fb19796e631f74e68f50196e85d0ceb710e9797
Jay M Gonzalez <jaimungon@gmail.com>
2022-10-26 09:41:36 +0200

    deploy app + new url for nft

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = '0xA0b4F2b717c1fCC4F803258ACC0314e159F7a5Fd'
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/'
+const METADATA_URL = 'https://nft-collection-crypto-devs.vercel.app/api/'
 
 module.exports = {
   WHITELIST_CONTRACT_ADDRESS,
